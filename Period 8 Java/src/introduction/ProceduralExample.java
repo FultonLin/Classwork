c28c624f675a122e99e003fcc5a973b2e043ab3e
Student8 <Student8@10.8.33.177>
2016-09-15 14:03:29 -0400

    asdf

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -4,6 +4,10 @@ public class ProceduralExample {
   public static void main(String[] args){
 	String[] names={"Jillian","Joseph","Jordan"};
 	String[] years={"Freshman","Junior","Senior"};
+	for(int i = 0; i < 3; i++)
+	{
+		System.out.println("Hi, my name is " + names[i] + " and I am a " + years[i] + ".");
+	}
 	System.out.println("Hi, my name is " + names[0] + " and I am a " + years[0] + ".");
 	System.out.println("Hi, my name is " + names[1] + " and I am a " + years[1] + ".");
 	System.out.println("Hi, my name is " + names[2] + " and I am a " + years[2] + ".");
