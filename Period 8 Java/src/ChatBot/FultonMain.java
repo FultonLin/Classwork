856d00ad5ff60d2ea1e0bdef43f1beab1c8b2abd
Student8 <Student8@10.8.33.177>
2016-09-27 14:04:08 -0400

    asd

--- Period 8 Java/src/ChatBot/FultonMain.java
+++ Period 8 Java/src/ChatBot/FultonMain.java
@@ -59,9 +59,12 @@ public class FultonMain {
 		//make lowercase
 		searchString = searchString.toLowerCase();
 		keyword = keyword.toLowerCase();
+		System.out.println("The phrase is "+searchString);
+		System.out.println("The keyword is "+keyword);
 		
 		//find first position of key word
-		int psn = searchString.indexOf(0);
+		int psn = searchString.indexOf(keyword);
+		System.out.println("The keyword was found at "+psn);
 		
 		//keep searching until context keyword found
 		while(psn >= 0){
@@ -71,21 +74,59 @@ public class FultonMain {
 			//check character in front, if it exists
 			if(psn > 0){
 				before = searchString.substring(psn-1, psn);
+				System.out.println("The character before is "+before);
 			}
 			//check if there is a character after the keyword
+			
 			if(psn+ keyword.length() < searchString.length()){
 				after = searchString.substring(psn + keyword.length(), psn + keyword.length()+1);
+				System.out.println("The character after is "+after);
 			}
-			if(before.compareTo("a") < 0 && after.compareTo("a") < 0){
+			
+			if(before.compareTo("a") < 0 && after.compareTo("a") < 0 && noNegations(searchString,psn)){
+				System.out.println("Found "+keyword+" at "+psn);
 				return psn;
 			}
+			
 			else{
 				//psn + 1 is one space after our current psn, so this finds the NEXT word.
 				psn = searchString.indexOf(keyword,psn+1);
+				System.out.println("Did not find "+keyword+", checking position "+psn);
 			}
 		}
 		
 		return -1;
+	}
+	
+	/**
+	 * This is a HELPER METHOD. A helper method is a method designed for "helping" a larger method.
+	 * because of this, helper methods are generally private because they are only used by the methods
+	 * the are helping. Also, when you do you project, I expect to see helper methods because they 
+	 * also make code more READABLE
+	 * @param searchString (always lowercase)
+	 * @param psn
+	 * @return "true" if there is no negation words in front of the psn
+	 */
+	private static boolean noNegations(String searchString, int psn) {
+		//check to see if the word "no" is in front of psn
+		//check to see if there are 3 spaces in front then check to see if "no " is there
+		if(psn - 3 >= 0 && searchString.substring(psn-3,psn).equals("no ")){
+			return false;
+		}
+		
+		if(psn - 4 >= 0 && searchString.substring(psn-4,psn).equals("not ")){
+			return false;
+		}
+		
+		if(psn - 6 >= 0 && searchString.substring(psn-6,psn).equals("never ")){
+			return false;
+		}
+		
+		if(psn - 4 >= 0 && searchString.substring(psn-4,psn).equals("n't ")){
+			return false;
+		}
+
+		return true;
 	}
 
 	public static String promptInput() {
