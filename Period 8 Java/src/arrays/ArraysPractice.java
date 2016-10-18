f9b99c4488f8ed70a35edb5661fe7ac7ea578c35
Student8 <Student8@10.8.33.177>
2016-10-18 14:03:55 -0400

    asdasdasdasdasd

--- Period 8 Java/src/arrays/ArraysPractice.java
+++ Period 8 Java/src/arrays/ArraysPractice.java
@@ -15,19 +15,85 @@ public class ArraysPractice {
 	public static void main(String[] args) {
 		//how do you time a process?
 		long currentTime = System.currentTimeMillis();
+		int diceNumber = 4;
 		
+		int[] fiftyNumbers = new int[50000];
+//		populate(fiftyNumbers);
+//		printInt(fiftyNumbers);
+//		randomize(fiftyNumbers,50);
+//		printInt(fiftyNumbers);
+		rollDice(fiftyNumbers,diceNumber);
+//		//count each die roll and provide a percentage
+		countResult(fiftyNumbers,diceNumber);
+		
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println("The process took " + (endTime-currentTime) + "ms." );
+	}
+	
+	private static void countResult(int[] s,int numberOfDice) {
+		int[] numbers = new int[numberOfDice*6];
+		for(int n: s){
+			numbers[n-1] = numbers[n-1] + 1;
+		}
+		for(int i = numberOfDice-1; i < numbers.length; i++){
+			System.out.println((i+1) + " was rolled "+ 100*numbers[i]/s.length + "%");
+		}
+	}
+
+	private static void rollDice(int[] s, int numberOfDice) {
+		for(int i = 0; i < s.length; i++){
+			int dice = 0;
+			for(int j = 0; j < numberOfDice; j++){
+				dice = dice + (int) (1+6*Math.random());
+			}
+			s[i] = dice;
+		}
+	}
+
+	
+
+	@SuppressWarnings("unused")
+	private static void randomize(int[] s, int max) {
+		for(int i = 0; i < s.length; i++){
+			s[i] = 1+(int)(max*Math.random());
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static void populate(int[] s) {
+		for(int i = 0; i < s.length; i++){
+			s[i] = i+1;
+		}
+	}
+
+	private static void printInt(int[]s){
+		for(int i = 0; i < s.length; i++){
+			System.out.println(s[i]);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	@SuppressWarnings("unused")
+	private void demonstratePassByValue(){
 		String[] someStrings = new String[1000];
 		standardPopulate(someStrings);
 		String s =someStrings[999];
 		makeSpecial(s);
+		someStrings[999] = getASpecialString();
 		print(someStrings);
 		
 //		initializingArraysExample();
-		long endTime = System.currentTimeMillis();
-		System.out.println("The process took " + (endTime-currentTime) + "ms." );
-	
 	}
 	
+	private static String getASpecialString() {
+		return "THIS STRING IS SPECIAL";
+	}
+
 	private static void makeSpecial(String s) {
 		s = "THIS STRING IS SPECIAL!";
 	}
@@ -44,12 +110,30 @@ public class ArraysPractice {
 		}
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void initializingArraysExample(){
 		//If you want to put different primitive types into an array, you must use their wrapper class
 		
 		//two different ways to instantiate an array
 		boolean[] boos1 = new boolean[3];
 		//this can ONLY be done at the declaration because it sets size AND content
+		@SuppressWarnings("unused")
 		boolean[] boos2 = {false,false,false}; 
 		//this does NOT work
 		//boos3 = {false,false,false};
@@ -74,6 +158,7 @@ public class ArraysPractice {
 		}
 		
 		//OBJECT ARRAYS
+		@SuppressWarnings("unused")
 		String[] someStrings2 = {"a","b","c"};
 		String[] someStrings1 = new String[3];
 		//you can do this to instantiate the elements...
