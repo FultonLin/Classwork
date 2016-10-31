b0af42d5cbf740d79a063b1e59a3aa92e4e5865b
Student8 <Student8@10.8.33.177>
2016-10-31 14:01:41 -0400

    asdasdasdas

---
+++ Period 8 Java/src/CaveExplorer/CaveRoomPd8.java
@@ -0,0 +1,17 @@
+package CaveExplorer;
+
+public class CaveRoomPd8{
+
+	public CaveRoomPd8(String description) {
+		
+	}
+
+	public void enter() {
+		
+	}
+
+	public void setConnection(CaveRoomPd8 caveRoomPd8) {
+		
+	}
+
+}
