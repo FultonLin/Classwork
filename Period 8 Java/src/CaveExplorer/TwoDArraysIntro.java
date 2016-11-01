a7dc971f163e6d084eff152b8c8fc0d2ba6b7b51
Student8 <Student8@10.8.33.177>
2016-11-01 14:04:13 -0400

    asdasdasd

--- Period 8 Java/src/CaveExplorer/TwoDArraysIntro.java
+++ Period 8 Java/src/CaveExplorer/TwoDArraysIntro.java
@@ -5,6 +5,37 @@ import java.util.Arrays;
 public class TwoDArraysIntro {
 
 	public static void main(String[] args) {
+		String[][] pic = new String[10][8];
+		for(int row = 0; row < pic.length; row++){
+			//populate with coordinates
+			for(int col = 0; col < pic[row].length; col++){
+				pic[row][col] = "";
+			}
+		}
+		//sun
+		pic[1][2] = "-";
+		pic[2][3] = "|";
+		pic[1][3] = "0";
+		pic[2][3] = "|";
+		pic[1][4] = "-";
+
+		
+		//grassy field
+		for(int row = 5; row < pic.length; row++){
+			for(int col = 0; col < pic[row].length; col++){
+				pic[row][col] = "w";
+			}
+		}
+		
+		for(String[] row : pic){
+			for(String col: row){
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+	}
+	  
+	public static void intro(){
 		String[] xox = {"x","o","x","o","x"};
 		System.out.println(Arrays.toString(xox));
 		//a ID array prints a horizontal string
@@ -24,5 +55,4 @@ public class TwoDArraysIntro {
 			System.out.println(Arrays.toString(row));
 		}
 	}
-
 }
