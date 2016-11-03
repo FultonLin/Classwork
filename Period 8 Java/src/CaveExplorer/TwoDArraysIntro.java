5b4bf4dc753d0976d1158b812f28dafa62df66c7
Student8 <Student8@10.8.33.177>
2016-11-03 14:03:10 -0400

    asdasdasd

--- Period 8 Java/src/CaveExplorer/TwoDArraysIntro.java
+++ Period 8 Java/src/CaveExplorer/TwoDArraysIntro.java
@@ -1,11 +1,119 @@
 package CaveExplorer;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TwoDArraysIntro {
+	public static Scanner in = new Scanner(System.in);
+	static String[][] arr2D;
+	static String[][] pic;
+	static int starti;
+	static int startj;
+	static int treasurei;
+	static int treasurej;
 
+	
 	public static void main(String[] args) {
+		arr2D = new String[5][5];
+		pic = new String[5][5];
+		for(int row = 0; row < pic.length; row++){
+			for(int col = 0; col < pic[row].length; col++){
+				pic[row][col] = " ";
+			}
+		}
+		for(int row = 0; row < arr2D.length; row++){
+			for(int col = 0; col < arr2D[row].length; col++){
+				arr2D[row][col] = "(" + row + ", " + col + ")";
+			}
+		}
+		starti = 2;
+		startj = 2;
+		treasurei = 4;
+		treasurej = 3;
+		startExploring();
 		
+//		String[][] grid = new String [15][15];
+//		makeGrid(grid);
+//		printPic(grid);
+	}
+	
+	private static void startExploring() {
+		while(true){
+			pic[interpretInput(pic)[0]][interpretInput(pic)[1]] = "x";
+			printPic(pic);
+			System.out.println("You are in room" + arr2D[starti][startj] + ".");
+			if(starti == treasurei && startj == treasurej){
+				break;
+			}
+			System.out.println("What do you want to do?");
+			String input = in.nextLine();
+			int[] newCoordinates = interpretInput(input);
+			starti = newCoordinates[0];
+			startj = newCoordinates[1];
+		}
+		System.out.println("Congratulations! You found the treasure!!");
+	}
+
+	private static int[] interpretInput(String input) {
+		//verify input is valid
+		while(!isValid(input)){
+			System.out.println("Sorry, in this game, you can only use the w, a, s, d controls.");
+			System.out.println("Tell me again what you would like to do.");
+			input = in.nextLine();
+		}
+		int currenti = starti;
+		int currentj = startj;
+		input = input.toLowerCase();
+		if(input.equals("w")){
+			currenti--;
+		}if(input.equals("s")){
+			currenti++;
+		}if(input.equals("a")){
+			currentj--;
+		}if(input.equals("d")){
+			currentj++;
+		}
+		int[] newCoordinates = {starti,startj};
+		if(currenti >= 0 && currenti < arr2D.length && currentj >= 0 && currentj < arr2D[0].length){
+			newCoordinates[0] = currenti;
+			newCoordinates[1] = currentj;
+		}else{
+			System.out.println("Sorry, you have reached the edge of the known universe."
+					+ " You may go no farther in that direction.");
+		}
+		return newCoordinates;
+	}
+
+	private static boolean isValid(String input) {
+		String[] validKeys = {"w","a","s","d"};
+		for(String key: validKeys){
+			if(input.toLowerCase().equals(key)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void makeGrid(String[][] grid) {
+		for(int row = 1; row < grid.length-1; row++){
+			for(int col = 0; col < grid[row].length; col++){
+				grid[row][col] = "|  ";
+			}
+		}
+		
+		for(int row = 0; row < grid.length; row++){
+			for(int col = 0; col < grid[row].length; col++){
+				if(grid[row][col] == null){
+					grid[row][col] = "___";
+				}
+				if(grid[row][col] == " "){
+					grid[row][col] = "_";
+				}
+			}
+		}
+	}
+	
+	public static void mines(){
 		boolean[][] mines = new boolean[6][6];
 		plantMines(mines);
 		String[][] field = createField(mines);
@@ -26,13 +134,39 @@ public class TwoDArraysIntro {
 	}
 
 	private static String countNearby(boolean[][] mines, int row, int col) {
-		for(int r = row - 1; r <= row + 1; r++){
-			for(int c = col - 1; c <= col + 1; c++){
-				if(r >= 0 && r < mines.length && c >= 0 && c < mines[0].length){
-					
-				}
-			}
-		}
+//		for(int r = row - 1; r <= row + 1; r++){
+//			for(int c = col - 1; c <= col + 1; c++){
+//				if(r >= 0 && r < mines.length && c >= 0 && c < mines[0].length){
+//					
+//				}
+//			}
+//		}
+		
+		
+//		//this method ONlY considers actual elements
+//		int count = 0;
+//		for(int r = 0; r < mines.length; r++){
+//			for(int c = 0; c < mines[r].length; c++){
+//				if(Math.abs(r-row) + Math.abs(c-col) == 1 && mines[r][c]){
+//					count++;
+//				}
+//			}
+//		}
+//		return ""+count;
+		
+		//this method to allow you to be most specific for example you only want north and east
+		int count = 0;
+		count += isValidAndTrue(mines, row-1, col);
+		count += isValidAndTrue(mines, row+1, col);
+		count += isValidAndTrue(mines, row, col-1);
+		count += isValidAndTrue(mines, row, col+1);
+		return ""+count;
+	}
+
+	private static int isValidAndTrue(boolean[][] mines, int i, int j) {
+		if(i >= 0 && i < mines.length && j >= 0 && j < mines[0].length && mines[i][j])
+			return 1;
+		else return 0;
 	}
 
 	private static void plantMines(boolean[][] mines) {
