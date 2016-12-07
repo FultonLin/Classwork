b2c8ab332e221f3734582c30a270d3ab44ed4978
Student8 <Student8@10.8.33.177>
2016-12-07 14:04:23 -0500

    12.123213123

--- Period 8 Java/src/guiPractice/GUIApplication.java
+++ Period 8 Java/src/guiPractice/GUIApplication.java
@@ -1,9 +1,13 @@
 package guiPractice;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class GUIApplication extends JFrame{
 
+	private Screen currentScreen;
+	
 	// main method for practice only
 	public static void main(String[] args) {
 		new GUIApplication();
@@ -17,6 +21,16 @@ public class GUIApplication extends JFrame{
 		int width = 600;
 		int height = 400;
 		setBounds(x,y,width,height);
+		initScreen();
 		setVisible(true);
+	}
+	
+	protected void initScreen(){
+		Screen startScreen = new Screen(getWidth(), getHeight());
+		currentScreen = startScreen;
+	}
+	
+	public void paint(Graphics g){
+		g.drawImage(currentScreen.getImage(), 0, 0, null);
 	}
 }
--- Period 8 Java/src/guiPractice/Screen.java
+++ Period 8 Java/src/guiPractice/Screen.java
@@ -1,5 +1,6 @@
 package guiPractice;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class Screen {
@@ -14,6 +15,17 @@ public class Screen {
 	}
 
 	private void initImage() {
-		
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		update();
+	}
+	
+	public void update() {
+		//this is where you draw stuff
+		Graphics2D g = image.createGraphics();
+		g.drawString("Hello", 40, 40);
+	}
+	
+	public BufferedImage getImage(){
+		return image;
 	}
 }
