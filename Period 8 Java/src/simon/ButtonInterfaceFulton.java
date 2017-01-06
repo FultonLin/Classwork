f0e884e0768902fef04702fe221e4fcbffe24479
FultonLin <Flin5289@bths.edu>
2017-01-05 22:27:18 -0500

    Update ButtonInterfaceFulton.java

--- Period 8 Java/src/simon/ButtonInterfaceFulton.java
+++ Period 8 Java/src/simon/ButtonInterfaceFulton.java
@@ -1,7 +1,18 @@
 package simon;
 
+import guiPractice.components.Action;
 import guiPractice.components.Clickable;
 
 public interface ButtonInterfaceFulton extends Clickable {
+	void setColor(Color color);
+
+	void setX(int i);
+
+	void setY(int i);
 	
+	void setAction(Action a);
+
+	void highlight();
+
+	void dim();
 }
